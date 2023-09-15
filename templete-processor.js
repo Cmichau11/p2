1efde52201296e3a8d015d2b1aaf60aa8dde7912
Cmichau11 <143210597+Cmichau11@users.noreply.github.com>
2023-09-15 10:06:05 -0400

    Create templete-processor.js

---
+++ templete-processor.js
@@ -0,0 +1,21 @@
+// New Templete Processer Class
+function TempleteProcessor(template){
+  this.template = template;
+}
+
+// Create the fillin method
+TempleteProcessor.fillin = function (dictionary){
+  // Match string in form {{property}}
+  var property = /{{()}}/;
+
+  var templeteFill = this.template.replace(property, function (match, property) {
+
+// Check if dictionary object, if not, replace with empty string
+  if (dictionary.hasOwnProperty(property)) {
+  return dictionary[property];
+  } else {
+    return '';
+  }
+  return templeteFill;
+}
+  
