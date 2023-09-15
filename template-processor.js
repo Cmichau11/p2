a8454e05aa3c29db97fe1bca3acd0554b8563121
Cmichau11 <143210597+Cmichau11@users.noreply.github.com>
2023-09-15 13:00:51 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -4,9 +4,9 @@ function TemplateProcessor(template){
 }
 
 // Create the fillin method
-TemplateProcessor.prototype.fillin = function (dictionary){
+TemplateProcessor.prototype.fillIn = function (dictionary){
   // Match string in form {{property}}
-  var property = /{{(.*?)}}/;
+  var property = /{{()}}/;
 // Preforming a replacement option on this.template string if it does not follow the form {{property}} template.
   var templeteFill = this.template.replace(property, function (match, property) {
 
