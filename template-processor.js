b13b3afa3335b69084e9cb6e3ecdd1961dc25ae3
Cmichau11 <143210597+Cmichau11@users.noreply.github.com>
2023-09-15 10:26:25 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -8,7 +8,7 @@ TempleteProcessor.fillin = function (dictionary){
   // Match string in form {{property}}
   var property = /{{()}}/;
 
-  var templeteFill = this.template.replace(property, function (match, property) {
+  var templeteFill = this.template.replace(property), function (match, property) {
 
 // Check if dictionary object, if not, replace with empty string
   if (dictionary.hasOwnProperty(property)) {
